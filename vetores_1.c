527ed26954d416ed33271a50070399abe27b5af1
Erick <hdias2331@gmail.com>
2019-08-21 15:37:10 -0300

    Vetores e .gitignore

---
+++ vetores_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+	int main (void) {
+
+		int i, V[10], soma = 0;
+
+		printf("Digite 10 valores\n");
+
+		for (i = 0; i < 10; i++) {
+		scanf("%d", &V[i]);
+		
+		}
+		
+		if (V[i] == 30) {
+			printf("O numero 30 esta na posição %d \n" i);
+		}
+		system("pause");
+		return 0;
+	}
